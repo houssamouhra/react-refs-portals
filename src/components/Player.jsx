2ba97be74c104a8eb0918b2618020942eb1a1023
Houssam Ouhra <houssamblur04@gmail.com>
2025-10-03 00:14:43 +0100

    feat(Header): improve player header section

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,17 @@
 export default function Player() {
   return (
     <section id='player'>
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type='text' />
-        <button>Set Name</button>
-      </p>
+      <header id='content'>
+        <h1>
+          The <em>almost</em> final countdown
+        </h1>
+        <p>Stop the timer once you estimate that time is (almost) up</p>
+        <h2>Welcome unknown entity</h2>
+        <p>
+          <input type='text' />
+          <button>Set Name</button>
+        </p>
+      </header>
     </section>
   );
 }
